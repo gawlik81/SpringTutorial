5fec18ee2f6042b9e57ef91a5ce5e41a26d4ebf4
Paweł Miernik <pawel.miernik81@gmail.com>
2017-04-13 12:45:53 +0200

    Obsluga scope = "singleton"

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -13,7 +13,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
    public static void main(String[] args) {
       ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-      obj.getMessage();
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+      //objA.setMessage("I'm object A");
+      objA.getMessage();
+
+      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+      objA.setMessage("I'm object A");
+      objB.getMessage();
    }
 }
