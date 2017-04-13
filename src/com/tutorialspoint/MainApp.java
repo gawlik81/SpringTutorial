1bb40d35019622b6d21e48f15ff83c96b1482a29
Paweł Miernik <pawel.miernik81@gmail.com>
2017-04-13 13:08:24 +0200

    init-method + destroy-method

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -7,12 +7,13 @@
  */
 package com.tutorialspoint;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      AbstractApplicationContext  context = new ClassPathXmlApplicationContext("Beans.xml");
+      context.registerShutdownHook();
       HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
 
       //objA.setMessage("I'm object A");
@@ -20,6 +21,7 @@ public class MainApp {
 
       HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
       objA.setMessage("I'm object A");
+      
       objB.getMessage();
       
       HelloWorld objC = (HelloWorld) context.getBean("helloWorld2");
